09b2cbbad5a3b177913b45e67bbcace41f87ff53
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-14 02:27:19 -0300

    ConstructorWithDefaultValues.

--- org/mybookshelf/Book.java
+++ org/mybookshelf/Book.java
@@ -11,9 +11,18 @@ public class Book {
 These variables are associated with individual instances or objects of the class, hence the name "instance variables." */
 
 // Constructor with parameters
-public Book (String title, String author, String genre, boolean isRead) {
+public Book(String title, String author, String genre, boolean isRead) {
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.isRead = isRead;
+}
+
+// Default constructor
+public Book() {
+    // Default values
+    this.title = " ";
+    this.author = " ";
+    this.genre = " ";
+    this.isRead = false;
 }
