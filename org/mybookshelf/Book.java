0fbc3bbe896abb5b7e0c5690e78b1419bdd6b27a
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-14 02:10:30 -0300

    ConstructorWithParameters.

--- org/mybookshelf/Book.java
+++ org/mybookshelf/Book.java
@@ -9,3 +9,8 @@ public class Book {
 
 /* The term "instance variables" refers to variables that are declared within a class but outside of any method. 
 These variables are associated with individual instances or objects of the class, hence the name "instance variables." */
+
+// Constructor with parameters
+public Book (String title, String author, String genre, boolean isRead) {
+
+}
