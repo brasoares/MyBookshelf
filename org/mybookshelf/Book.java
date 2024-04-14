0f4b86e402a3a3682e71b5f2def24590f7243bce
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-13 23:36:16 -0300

    Instance variables

---
+++ org/mybookshelf/Book.java
@@ -0,0 +1,9 @@
+package org.mybookshelf;
+
+public class Book {
+    private String title;
+    private String author;
+}
+
+/* The term "instance variables" refers to variables that are declared within a class but outside of any method. 
+These variables are associated with individual instances or objects of the class, hence the name "instance variables." */
