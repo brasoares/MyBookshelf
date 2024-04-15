0a5476c45d4c7da0634f4c9c234e310e27bf7522
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-15 03:22:12 -0300

    CodeBeautified!

--- org/mybookshelf/Book.java
+++ org/mybookshelf/Book.java
@@ -1,10 +1,10 @@
 package org.mybookshelf;
 
 public class Book {
-    private String title;
-    private String author;
-    private String genre;
-    private boolean isRead;
+  private String title;
+  private String author;
+  private String genre;
+  private boolean isRead;
 }
 
 /* The term "instance variables" refers to variables that are declared within a class but outside of any method. 
@@ -12,53 +12,53 @@ These variables are associated with individual instances or objects of the class
 
 // Constructor with parameters
 public Book(String title, String author, String genre, boolean isRead) {
-    this.title = title;
-    this.author = author;
-    this.genre = genre;
-    this.isRead = isRead;
+  this.title = title;
+  this.author = author;
+  this.genre = genre;
+  this.isRead = isRead;
 }
 
 // Default constructor
 public Book() {
-    // Default values
-    this.title = " ";
-    this.author = " ";
-    this.genre = " ";
-    this.isRead = false;
+  // Default values
+  this.title = " ";
+  this.author = " ";
+  this.genre = " ";
+  this.isRead = false;
 }
 
 // Getters and Setters for title
 public String getTitle() {
-    return title;
+  return title;
 }
 
-public void setTitle (String title) {
-    this.title = title;
+public void setTitle(String title) {
+  this.title = title;
 }
 
 // Getters and Setters for author
 public String getAuthor() {
-    return author;
+  return author;
 }
 
-public void setAuthor (String author) {
-    this.author = author;
+public void setAuthor(String author) {
+  this.author = author;
 }
 
 // Getters and Setters for genre
 public String getGenre() {
-    return genre;
+  return genre;
 }
 
-public void setGenre (String genre) {
-    this.genre = genre;
+public void setGenre(String genre) {
+  this.genre = genre;
 }
 
 // Getters and Setters for isRead
 public boolean isRead() {
-    return isRead;
+  return isRead;
 }
 
 public void setRead(boolean isRead) {
-    this.isRead = isRead;
+  this.isRead = isRead;
 }
