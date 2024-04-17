3105d1e7ac4f22c7ceb6df5c4ac592167a0783f1
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-16 22:59:30 -0300

    BookCollection

--- org/mybookshelf/BookCollection.java
+++ org/mybookshelf/BookCollection.java
@@ -3,4 +3,8 @@ package org.mybookshelf;
 public class BookCollection {
     private Book[] books;
     private int numberOfBooks;
+    private String collectionName;
+    private String[] tags;
+    private String ownerUserID;
+    private long lastUpdatedTimestamp;
 }
