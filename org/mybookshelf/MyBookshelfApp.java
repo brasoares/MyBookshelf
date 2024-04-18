b01cfc0c5db6f7c758eddae666fe8ee1bfe0c069
Henoc Soares <henocsoares@hotmail.com.br>
2024-04-17 23:08:44 -0300

    Update MyBookshelfApp.java

--- org/mybookshelf/MyBookshelfApp.java
+++ org/mybookshelf/MyBookshelfApp.java
@@ -1,7 +1,7 @@
 public class MyBookshelfApp {
     public static void main(String[] args) {
-        // Main method - Entry point of the application
+      
         System.out.println("Welcome to MyBookshelf!");
-        // Additional code to start the app goes here
+        
     }
 }
